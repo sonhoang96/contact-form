49d67749510d548b305fd75941fdb7fa84e3366d
sonhoang96 <sonhoangit96@gmail.com>
2020-07-29 10:43:40 +0700

    add events and finish code

--- js/index.js
+++ js/index.js
@@ -1,1 +1,25 @@
-console.log("JAVASCRIPT")
+$(document).ready(function() {
+	let inputs = $('.form_info').find('input');
+	let message = $('.form_info').find('textarea');
+	//message, phone, email, name
+	let text = $('.form_info').find('p');
+	
+	inputs.focus(function(event) {
+		$(this).parent().find('p').addClass('typing');
+	});
+	inputs.blur(function(event) {
+		let val = event.target.value;
+		if(val == ''){
+			$(this).parent().find('p').removeClass('typing');
+		}
+	});
+	message.focus(function(event) {
+		$(this).parent().find('p').addClass('typing');
+	});
+	message.blur(function(event) {
+		let val = event.target.value;
+		if(val == ''){
+			$(this).parent().find('p').removeClass('typing');
+		}
+	});
+});
